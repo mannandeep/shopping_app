8eb3117ba452a0ac985038300f6c9206f40b3d1e
mannandeep <44285041+mannandeep@users.noreply.github.com>
2020-03-30 23:51:03 +0530

    Update cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -5,21 +5,17 @@ from coupons.models import Coupon
 
 class Cart(object):
     def __init__(self,request):
-        '''
-        Initialize the cart
-        '''
+        
         self.session = request.session #storing the current session
         cart= self.session.get(settings.CART_SESSION_ID) #get method
         if not cart:
-            # save an empty cart in the session
+           
             cart = self.session[settings.CART_SESSION_ID]={}
         self.cart = cart
         self.coupon_id=self.session.get('coupon_id')
 
     def add(self, product, quantity=1,update_quantity=False):
-        '''
-        Add a product to the cart or update its quantity
-        '''
+       
 
         product_id=str(product.id)
         if product_id not in self.cart:
@@ -34,17 +30,13 @@ class Cart(object):
         self.session.modified =True
 
     def remove(self,product):
-        '''
-        Remove product from cart.
-        '''
+        
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
     def __iter__(self):
-        '''
-        Iterate over the items in the cart and get the products from the database
-        '''
+        
 
         product_ids = self.cart.keys()
         #get the product objects and add them to the cart
@@ -69,7 +61,7 @@ class Cart(object):
         return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
 
     def clear(self):
-        #remove cart from session
+      
         del self.session[settings.CART_SESSION_ID]
         self.save()
 
